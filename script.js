f7984220cc03be9c9e6efc03b769131f2ba9f1fe
Thony <bellotony229@gmail.com>
2022-09-30 11:58:56 +0100

    Added functionality

---
+++ script.js
@@ -0,0 +1,115 @@
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function userInterface() {}
+//Add book to list
+userInterface.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  //create tr element
+  const row = document.createElement("tr");
+  //insert cols
+  row.innerHTML = `<td>${book.title} </td>
+                   <td>${book.author} </td>
+                   <td>${book.pages} </td>
+                   <td>${book.read} </td>
+                   <td><a href = '#' class='delete'>X<a></td>`;
+
+  list.appendChild(row);
+  list.style.fontWeight = "bold";
+};
+// show Alert
+userInterface.prototype.showAlert = function (message, className) {
+  //create div
+  const div = document.createElement("div");
+  //Add classes
+  div.className = `alert ${className}`;
+  // Add text
+  div.appendChild(document.createTextNode(message));
+  // Get parent
+  const container = document.querySelector(".container");
+  const form = document.querySelector("form");
+  container.insertBefore(div, form);
+
+  //set time out after 3 sec
+  setTimeout(function () {
+    document.querySelector('.alert').remove();
+  }, 3000);
+};
+
+// Delete Book
+userInterface.prototype.deleteBook =function(target){
+  if(target.className === 'delete'){
+  target.parentElement.parentElement.remove();
+  }
+}
+
+
+
+userInterface.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("pages").value = "";
+  document.getElementById("read").value = "";
+};
+
+// Eventlistener for add book
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  // getting form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    pages = document.getElementById("pages").value;
+
+  //selecting the radio buttons value
+  let read_status = () => {
+    if (document.getElementById("read").checked) {
+      return document.getElementById("read").value;
+    } else {
+      return document.getElementById("un-read").value;
+    }
+  };
+  let read = read_status();
+
+  // instantiating a book
+  const book = new Book(title, author, pages, read);
+
+  // instantiating userInterface
+  const ui = new userInterface();
+
+  //validate
+  if (title === "" || author === "" || pages === "" || read === "") {
+    //error alert
+    ui.showAlert("please fill in all fields", "error");
+  } else {
+    //Add book to list
+    ui.addBookToList(book);
+    // show success
+    ui.showAlert('Book Sucessfully Added!', 'success')
+
+    // clear fields
+
+    ui.clearFields();
+  }
+  // console.log(read);
+
+  e.preventDefault();
+});
+
+
+// Eventlistener for delete book
+
+document.getElementById('book-list').addEventListener('click', function(e){
+
+// instantiating userInterface
+const ui = new userInterface();
+
+//delete book
+ui.deleteBook(e.target)
+
+//show alert after delete
+ui.showAlert('Book Removed', 'success')
+  e.preventDefault();
+})
